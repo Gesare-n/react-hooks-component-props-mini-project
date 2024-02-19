864c9aa1182b6495b2d6161225d5f9a3b538b5ba
Gesare-n <114504698+Gesare-n@users.noreply.github.com>
2024-02-19 06:48:18 +0300

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About(props) {
+  const defaultImage = "https://via.placeholder.com/215";
+  return (
+    <aside>
+      <img
+        src={props.image || defaultImage}
+        alt="blog logo"
+      />
+      <p>{props.about}</p>
+    </aside>
+  );
+}
+
+export default About;
